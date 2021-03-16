b0800110bfeb3bacaf826e8509e6fa5309dbfcfe
gustaa23 <gustaa23@Gmail.com>
2021-03-15 21:03:01 -0300

    Add files via upload

---
+++ brincando-com-javascript/main.js
@@ -0,0 +1,104 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigador por clicar" ;
+}
+
+function redirecionar(){
+    window.open("https://github.com/gustaa23");
+    window.location.href = "https://github.com/gustaa23";
+}
+function redirecionar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+function load(){
+    alert("pagina carregada");
+}
+
+functionfuncaoChange(elemento){
+    console.log(elemento.value);
+}
+/*
+function redirecionar(elemento){
+    document.getElementById(mousemove).innerHTML = "Obrigado por passar o mouse";
+    alert("trocar texto");
+}
+
+function voltar(elemento){
+    document.getElementById(mousemove).innerHTML = "Passe o mouse aqui";
+}
+function clicou(){
+    document.getElementById("agradecimento");
+}
+function clicou(){
+    alert("Obrigado por clicar")
+}
+function soma(n1, n2){
+    return n1 + n2;
+}
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+        }
+        return validar;
+}
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+function soma(n1, n2){
+    return n1 + n2
+}
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(soma(5,10))
+alert(setReplace("Vai japâo","Japão","Brasil"));
+var d = mew Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+var count;
+far(count=0; count <= 5 ; count++){
+    alert(count);
+};
+var count = 5;
+while(count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+var idade = prompt("Qual sua idade");
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(fruta);
+alert(frutas[1].nome);
+var fruta = {nome:"maça", cor:"vermelho"};
+console.log(fruta.nome);
+alert(fruta.cor);
+var lista = ["maça", "péra", "laranja"];
+lista.push("uva")
+lista.pop
+console.log(lista[0])
+console.log(lista.toString())
+console.log(lista.join("  "))
+var nome = "gustavo caires da sila";
+var n1 = 5;
+var n2 = 3;
+var frase = "China tem o melhores jogadores em tudo";
+alert(nome + "tem" + idade + "anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(n1 = n2);
+console.log(frase.tolowerCase);
+alert(frase.replace("China","Brasil"));
+*/
